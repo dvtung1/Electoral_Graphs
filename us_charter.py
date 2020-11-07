71296fb88636e4cf40482a2475aeddc32d0ee3cd
arefmalek <malek.aref1@gmail.com>
2020-11-07 11:07:17 -0800

    added first 2 images

--- us_charter.py
+++ us_charter.py
@@ -12,10 +12,12 @@ us['whiteness lol'] = df_states['N.H.W. Percentage']
 us["demon time"] = df_states["E.C. Votes"] / df_states["Population"]
 
 f, ax = plt.subplots(1, figsize=(100, 100))
+
+#put an option for if people want the electoral votes / person
 us.plot(column='demon time', ax=ax, cmap='Greens')
 
-#f, ax = plt.subplots(2, figsize=(100, 100))
-us.plot(column="whiteness lol", ax=ax, cmap="coolwarm")
+# another option for if people want to see whiteness
+# us.plot(column="whiteness lol", ax=ax, cmap="coolwarm")
 
 
 plt.show()
