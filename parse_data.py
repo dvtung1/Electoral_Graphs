d1f104b16c20770fa4f9bd4420e411217d91665c
arefmalek <malek.aref1@gmail.com>
2020-11-06 20:06:46 -0800

    csv file with state data

--- parse_data.py
+++ parse_data.py
@@ -2,20 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 
-gdp = pd.read_csv("https://raw.githubusercontent.com/PitchInteractiveInc/tilegrams/master/data/us/population-by-state.csv", header=None)
-ev = pd.read_csv("https://raw.githubusercontent.com/PitchInteractiveInc/tilegrams/master/data/us/gdp-by-state.csv", header=None)
-pop = pd.read_csv("https://raw.githubusercontent.com/PitchInteractiveInc/tilegrams/master/data/us/population-by-state.csv", header=None)
-
-
-
-print(gdp.head())
-print(pop.head())
-print(ev.head())
-
-with open("statekey.json") as f:
-    info = json.load(f)
-
-# for value in info:
-#     print(value["name_short"])
-
-print(info)
+df = pd.read_csv("state_data")
+print(df.head())
